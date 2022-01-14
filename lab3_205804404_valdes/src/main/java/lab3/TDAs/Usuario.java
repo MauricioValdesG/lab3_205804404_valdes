c4469d549107debb676b23d12f217336b12317ab
MauricioValdesG <mauricio.valdes.g@usach.cl>
2022-01-14 17:01:25 -0300

    clase usuario

---
+++ lab3_205804404_valdes/src/main/java/lab3/TDAs/Usuario.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3.TDAs;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Usuario {
+    
+    //atributos
+    private static final AtomicInteger counter = new AtomicInteger();
+    private final int id;
+    private final Date fecha;
+    private final String Nombre;
+    private final String Contrasenia;
+
+    public Usuario(int id, Date fecha, String Nombre, String Contrasenia) {
+        this.id = id;
+        this.fecha = fecha;
+        this.Nombre = Nombre;
+        this.Contrasenia = Contrasenia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getContrasenia() {
+        return Contrasenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{"
+                + "id=" 
+                + id 
+                + ", fecha=" 
+                + fecha 
+                + ", Nombre=" 
+                + Nombre 
+                + ", Contrasenia=" 
+                + Contrasenia 
+                + '}';
+    }
+}
